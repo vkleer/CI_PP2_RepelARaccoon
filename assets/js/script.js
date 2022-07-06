728c44735cfc956b52e287b32e9df4c1db0a14ef
Vilayat Kleer <vkleer@hotmail.com>
2022-07-06 14:11:49 +0000

    Add incrementScore function to keep count of score

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,16 +18,20 @@ function createBushes() {
  * Spawn a raccoon
  */
  function spawnRaccoon() {
+    // Sets the lifespan of raccoon
     const spawnInterval = 2000;
     let bush = randomBush();
     let raccoon = document.createElement('div');
+    // Add raccoon class to div and remove empty class from bush
     raccoon.classList.add('raccoon');
     bush.classList.remove('empty');
 
+    // Add event listener to check for clicks
     bush.appendChild(raccoon);
     raccoon.addEventListener('click', function() {
         bush.removeChild(raccoon);
         bush.classList.add('empty');
+        incrementScore();
     })
 
     setTimeout(function() {
@@ -55,6 +59,11 @@ function randomBush() {
     }
     console.log(`Selected bush number ${randomBushNumber}`);
     return bushes[randomBushNumber];
+}
+// Gets the current score from the DOM and increments it by 1
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
 createBushes();
