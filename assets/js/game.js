ff5963fa79a5481159ad09b5dffaf2db5249dab3
Vilayat Kleer <vkleer@hotmail.com>
2022-07-13 17:43:32 +0000

    Renamed script.js to game.js, cleaned up code by adding doc strings and removing duplicate functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,34 +1,68 @@
-// Global variable to start/stop game and timer
+// Global variables used to start/end the game and set the game timer
 let GameState;
 let GameTimer;
-// Set the script to start the game on the start button
-// when the DOM has finished loading
+
+/**
+ * Adds an event listener to the document when its loaded.
+ * Grabs all elements with a class name of 'start-button', 'how-to-button' and 'home-button' and assigns each
+ * HTMLCollection to its own variable. 
+ * The 'startButtons' variable is looped through, each element being 
+ * assigned an event listener, calling the pickDifficulty function when clicked on.
+ * The 'howToButtons' and 'goBackButtons' variables are also looped through, each element being assigned an
+ * event listener, calling the showSection function when being clicked on instead.
+ */
 document.addEventListener('DOMContentLoaded', function() {
     let startButtons = document.getElementsByClassName('start-button');
     let howToButtons = document.getElementsByClassName('how-to-button');
     let goBackButtons = document.getElementsByClassName('home-button');
-    // Add startGame function to all buttons with start-button class
+
     for (let button of startButtons) {
        
         button.addEventListener('click', pickDifficulty);
     }
-    // Add howToPlay function to all buttons with how-to-button class
+
     for (let button of howToButtons) {
-        button.addEventListener('click', howToPlay);
+        button.addEventListener('click', function() {
+            showSection('how-to-play-section', 'How to Play');  
+        });
     }
-    // Add goBack function to all buttons with home-button class
+
     for (let button of goBackButtons) {
-        button.addEventListener('click', goBack);
+        button.addEventListener('click', function() {
+            showSection('home-section', `Gotta repel 'em all!`);
+        });
     }
+
 });
+/**
+ * Show the section that is given in the 'sectionId' parameter and set section title in the header equal 
+ * to the value given in the 'newTitle' parameter.
+ * Creates an array called 'sectionIds' containing all section id values in the index.html file and
+ * adds the 'hide' class to all sections except the one with the id of 'sectionId'
+ * @param {string} sectionId 
+ * @param {string} newTitle 
+ */
+function showSection(sectionId, newTitle) {
+    let sectionIds = ['home-section', 'difficulty-section', 'how-to-play-section', 'game-over-section', 'game-section'];
+    let sectionTitle = document.getElementById('section-title');
+
+    sectionTitle.innerText = newTitle;
+
+    for (let allSections of sectionIds) {
+        if (allSections === sectionId) {
+            document.getElementById(sectionId).classList.remove('hide');
+        } else {
+            document.getElementById(allSections).classList.add('hide');
+        }
+    }
+}
+/**
+ * Calls the showSection function and adds event listeners to the elements with an id of 'easy', 'normal'
+ * and 'hard'. If clicked on, the startGame function is called with different parameters depending on the
+ * difficulty that's been picked.
+ */
 function pickDifficulty() {
-    // Set the title of the section
-    document.getElementById('section-title').innerText = 'Choose your difficulty level';
-    // Hide the home-section and show the difficulty-section
-    document.getElementById('home-section').classList.add('hide');
-    document.getElementById('game-over-section').classList.add('hide');
-    document.getElementById('how-to-play-section').classList.add('hide');
-    document.getElementById('difficulty-section').classList.remove('hide');
+    showSection('difficulty-section', 'Choose your difficulty level');
     // Get the buttons to set difficulty with
     let easyButton = document.getElementById('easy');
     let normalButton = document.getElementById('normal');
@@ -46,69 +80,50 @@ function pickDifficulty() {
         startGame('Hard', 600, 9)
     });
 }
-/**
- * Shows game instructions
- */
-function howToPlay() {
-    // Set the title of the section
-    document.getElementById('section-title').innerText = 'How to Play'
-    // Hide the home-section and show the how-to-play-section
-    document.getElementById('home-section').classList.add('hide');
-    document.getElementById('game-over-section').classList.add('hide');
-    document.getElementById('how-to-play-section').classList.remove('hide');
-}
-function goBack() {
-    // Set the title of the section
-    document.getElementById('section-title').innerText = `Gotta repel 'em all!`;
-    // Hides the section the user is currently in and go back to home-section
-    document.getElementById('how-to-play-section').classList.add('hide');
-    document.getElementById('difficulty-section').classList.add('hide');
-    document.getElementById('home-section').classList.remove('hide');
-}
 /** 
- * 
- * Starts the game
+ * Clears 'GameState' and 'GameTimer' intervals and calls the showSection function. The 'GameTimer' variable 
+ * is set to an interval which calls the startTimer function every second. The 'GameState' variable is set to
+ * an interval which calls the spawnRaccoon function, the frequency depending on the 'lifeSpan' parameter.
+ * The elements with an id of 'time' and 'current-cheese' get assigned a value.
+ * If there are no elements with a class name of 'cheese' present, a div with the id of 'cheeses' is created
+ * and appended to the element with an id of 'game-section'. The createCheese function is called after.
+ * If there are no elements with a class name of 'bush' present, a div with the id of 'game-body' is created
+ * and appended to the element with an id of 'game-section'. The createBushes function is called after.
+ * @param {string} difficulty
+ * @param {number} lifeSpan
+ * @param {number} bushNumber
  */
-function startGame(difficulty, lifeSpan, bushNumber) { // Add parameters for time and difficulty level
-    // Clear GameState and GameTimer to avoid duplicating setInterval
+function startGame(difficulty, lifeSpan, bushNumber) {
     clearInterval(GameState);
     clearInterval(GameTimer);
-    // Set the title of the section
-    document.getElementById('section-title').innerText = `Difficulty: ${difficulty}`;
-    // Set time for game
-    let time = 5;
-    document.getElementById('time').innerText = time;
+    
+    showSection('game-section', `Difficulty: ${difficulty}`);
+
     GameTimer = setInterval(startTimer, 1000);
-    // Set raccoon spawn speed
     GameState = setInterval(function() {
         spawnRaccoon(lifeSpan)
     }, lifeSpan);
-    // Set cheese amount and show game-section
-    cheeseAmount = 5;
-    document.getElementById('current-cheese').innerText = cheeseAmount;
-    document.getElementById('difficulty-section').classList.add('hide');
-    document.getElementById('game-section').classList.remove('hide');
-    // Call createCheese is no cheese currently exists
+
+    document.getElementById('time').innerText = 30;
+    document.getElementById('current-cheese').innerText = 5;
+
     if (document.getElementsByClassName('cheese').length === 0) {
-        // Add cheeses to game-section
         let cheeses = document.createElement('div');
         cheeses.setAttribute('id', 'cheeses');
         document.getElementById('game-section').appendChild(cheeses);
-        // Call createCheese is no cheese currently exists
         createCheese();
     }
-    // Call createBushes if no bushes currently exist
+
     if (document.getElementsByClassName('bush').length === 0) {
-        // Add game-body to game-section 
         let gameBody = document.createElement('div');
         gameBody.setAttribute('id', 'game-body');
         document.getElementById('game-section').appendChild(gameBody);
-        // Call createBushes if no bushes currently exist
         createBushes(bushNumber);
     }
 }
 /**
- * Timer function
+ * Updates the element with the id of 'time' to show how much time is left before the game ends. 
+ * If the timer reaches 0, the winGame function is called with the parameter set to true.
  */
 function startTimer() {
     let oldTime = parseInt(document.getElementById('time').innerText);
@@ -120,23 +135,24 @@ function startTimer() {
     }
 }
 /**
- * Checks if the user won or lost
+ * Clears 'GameState' and 'GameTimer' intervals and calls the showSection function without providing
+ * a second parameter to keep showing the difficulty the game was played on.
+ * Removes the elements with an id of 'game-body' and 'cheeses' and changes the content in the 
+ * 'game-over-section' depending on whether the user won the game, lost the game, using the 'win' parameter.
+ * The elements with an id of 'score' and 'current-cheese' are used to provide the user with feedback.
+ * @param {boolean} win
  */
  function winGame(win) {
-    // Stop spawning raccoons and stop timer 
-   clearInterval(GameState);
-   clearInterval(GameTimer);
-   // Hide the game-section and show game-over-section
-   document.getElementById('game-section').classList.add('hide');
-   document.getElementById('game-over-section').classList.remove('hide');
-   // Remove game-body
-   document.getElementById('game-body').remove();
-   // Remove cheeses
-   document.getElementById('cheeses').remove();
-   // Get the score
+    clearInterval(GameState);
+    clearInterval(GameTimer);
+   
+    showSection('game-over-section');
+
+    document.getElementById('game-body').remove();
+    document.getElementById('cheeses').remove();
+
     let score = parseInt(document.getElementById('score').innerText);
     let cheeseLeft = parseInt(document.getElementById('current-cheese').innerText);
-
     if (win === true && cheeseLeft === 5) {
         document.getElementById('game-over-title').innerText = 'You Won!'
         document.getElementById('game-over-text').innerText = `
@@ -161,11 +177,13 @@ function startTimer() {
         It looks like you didn't repel any of the raccoons - perhaps read the game instructions and try again?`;
     }
     }
-   // After the score has been grabbed and displayed, set it back to 0
+
    document.getElementById('score').innerText = 0;
 }
 /**
- * Creates div elements where the racoons will spawn
+ * Creates div elements, the number of elements being equal to the 'bushNumber' parameter, 
+ * gives them the 'bush' class and appends them to the element with the id of 'game-body'
+ * @param {number} bushNumber
  */
 function createBushes(bushNumber) {
     let gameBody = document.getElementById('game-body');
@@ -180,7 +198,8 @@ function createBushes(bushNumber) {
     }
 }
 /**
- * Creates div elements that represent cheese
+ * Creates div elements, the number of elements being equal to the 'cheeseAmount' variable,
+ * gives them the 'cheese' class and appends them to the element with the id of 'cheeses'.
  */
  function createCheese() {
     let cheeses = document.getElementById('cheeses');
@@ -196,34 +215,39 @@ function createBushes(bushNumber) {
     }
 }
 /** 
- * Spawn a raccoon
+ * Calls the randomBush function on the 'bush' variable, creates a div element, gives it the 
+ * 'raccoon' class and appends it to 'bush'. Removes the 'empty' class from 'bush'.
+ * If the 'raccoon' element is clicked on or times out, it is removed and the 'empty' class is re-added 
+ * to 'bush'. If clicked on, the incrementScore function is called and if it times out the takeCheese
+ * function is called instead.
+ * The setTimeout uses the 'lifeSpan' parameter to set the timer.
+ * @param {number} lifeSpan
  */
- function spawnRaccoon(lifeSpan) { // set parameter to use for lifespan
+ function spawnRaccoon(lifeSpan) {
     let bush = randomBush();
     let raccoon = document.createElement('div');
-    // Add raccoon class to div and remove empty class from bush
+
     raccoon.classList.add('raccoon');
-    bush.classList.remove('empty');
-    // Add event listener to check for clicks
     bush.appendChild(raccoon);
+    bush.classList.remove('empty');
+
     raccoon.addEventListener('click', function() {
         bush.removeChild(raccoon);
         bush.classList.add('empty');
         incrementScore();
     });
-    // Removes raccoon from game after timer
+
     setTimeout(function() {  
         if (bush.contains(raccoon)) {
             bush.removeChild(raccoon);
             bush.classList.add('empty');
             takeCheese();
-        } else {
-            console.log('There are no raccoons!');
         }
     }, lifeSpan);
 }
 /** 
- * Select a random bush to spawn a raccoon in 
+ * Gets all elements with a class of 'bush' and returns one of the elements by random selection. If the
+ * selected element contains the 'empty' class, call the function again.
  */
 function randomBush() {
     let bushes = document.getElementsByClassName('bush');
@@ -232,21 +256,21 @@ function randomBush() {
     let currentBush = bushes[randomBushNumber];
     
     if (!currentBush.classList.contains('empty')) {
-        console.log('You already used this bush!');
         return randomBush();
     }
     console.log(`Selected bush number ${randomBushNumber}`);
     return bushes[randomBushNumber];
 }
 /** 
- * Gets the current score from the DOM and increments it by 1
+ * Updates the element with the 'score' id by incrementing its current value.
  * */ 
 function incrementScore() {
     let oldScore = parseInt(document.getElementById('score').innerText);
     document.getElementById('score').innerText = ++oldScore;
 }
 /**
- * Gets the current cheese from the DOM and reduces it by 1
+ * Updates the element with the 'current-cheese' id by subtracting its current value and calls the winGame 
+ * function with the parameter set to 'false' if the element is equal to 0.
  */
 function takeCheese() {
     let oldCheeseAmount = parseInt(document.getElementById('current-cheese').innerText);
